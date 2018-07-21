5391183692a2d64b7a6a3767d37033e15e8cfd09
mahmoud <mahmoudsalama805@gmail.com git config --global user.name mahmoud281994>
2018-07-21 17:24:41 +0200

    migration done

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResourse('/products','productController');
+Route::apiResource('/products','productController');
 Route::group(['prefix'=>'products'],function()
 {
-Route::apiResourse('{product}/reviews','reviewController');
+Route::apiResource('{product}/reviews','reviewController');
 });
