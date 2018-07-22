09df7176b80d929a7639a5a8114f7006939e60bc
mahmoud <mahmoudsalama805@gmail.com git config --global user.name mahmoud281994>
2018-07-22 17:38:53 +0200

    href

--- app/Http/Resources/product/productResource.php
+++ app/Http/Resources/product/productResource.php
@@ -18,8 +18,13 @@ class productResource extends JsonResource
             'name'=>$this->name,
             'describtion'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
+            'stock'=>$this->stock ==0?'out of stock':$this->stock,
             'discount'=>$this->discount,
+            'total_price'=>round((1-($this->discount/100))*$this->price),
+            'rating'=>$this->review->count()>0?round($this->review->sum('star')/$this->review->count()):'no rating yet',
+            'href'=>[
+                'reviews'=>route('reviews.index',$this->id)  
+            ]
         ];
 
     }
